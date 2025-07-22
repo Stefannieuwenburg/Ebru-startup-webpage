896325d8a32dd0e5ab5b354a5cd0177f1f15ab65
Stefan Nieuwenburg~ <Nieuwenburg.stefan@gmail.com>
2025-07-22 13:30:14 +0200

    commit main styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './Styles.css'; 
+import "Styles.css"; // Assuming you have a stylesheet for custom styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
