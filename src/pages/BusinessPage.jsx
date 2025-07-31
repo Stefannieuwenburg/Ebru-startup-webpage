f9f0039abc6ec7a941770729434c0b03b06b03e4
Stefan Nieuwenburg~ <Nieuwenburg.stefan@gmail.com>
2025-07-31 11:47:59 +0200

    commit pages

--- src/pages/BusinessPage.jsx
+++ src/pages/BusinessPage.jsx
@@ -15,45 +15,63 @@ const BusinessPage = () => {
                         </div>
                         <div>
                             <h2 className="text-lg text-[#3b3a2e] mb-6">
-                                <strong>For Business</strong>
+                                <strong>Event Energizer</strong>
                             </h2>
                             <p className="text-lg text-[#3b3a2e] mb-6">
-                                Empower your team with embodiment-based trainings designed to build emotional
-                                intelligence, resilience, and presence in the workplace. Through somatic awareness,
-                                breathwork, and conscious movement, these sessions help teams manage stress, improve
-                                focus, and foster authentic leadership in fast-paced environments.
-                            </p>
-                            <h3 className="mb-4 mt-4">
-                                <strong>Tools</strong>
-                            </h3>
-                            <ul className="text-lg text-[#3b3a2e] ">
-                                <li>Somatic Movement — ground and release tension</li>
-                                <li>Breathwork & Qigong — open energy flow and calm the nervous system</li>
-                                <li>Embodiment Rituals — ignite inner power and emotional freedom</li>
-                                <li>Relational Exercises — foster trust and authentic connection</li>
-                            </ul>
-
-                            <h3 className="mb-4 mt-4 ">
-                                <strong>Benefits </strong>
-                            </h3>
-                            <ul className="text-lg text-[#3b3a2e] ">
-                                <li>Deepen body, emotional, and intuitive connection</li>
-                                <li>Release held tension and stress</li>
-                                <li>Regulate nervous system for ease and clarity</li>
-                                <li>Feel supported in a safe collective space</li>
-                                <li>Access subconscious awareness and inner vitality</li>
-                            </ul>
-
-                            <p className="text-lg text-[#3b3a2e] mb-8 mt-4">
-                                These trainings are adaptable for team offsites, leadership retreats, and can be offered
-                                globally, both in-person and online. Let’s co-create a more conscious, connected, and
-                                thriving workplace — one breath, one body, one leader at a time.
+                                Step into a safe, supportive space created just for you. Rooted in Co-Active coaching,
+                                these tailored sessions integrate somatic tools, breathwork, and conscious movement to
+                                meet you exactly where you are. Whether you’re navigating emotional blocks, seeking
                             </p>
                             <Link
                                 to="/contact"
-                                className="bg-[#3b3a2e] text-white px-6 py-3 rounded-lg font-semibold hover:bg-[#2d2c22] transition-colors"
+                                className="bg-[#3b3a2e] text-white px-6 py-3 rounded-lg font-semibold hover:bg-[#2d2c22] transition-colors "
                             >
-                                Contact Us
+                                Read more
+                            </Link>
+                            <h2 className="text-lg text-[#3b3a2e] mb-6 mt-6">
+                                <strong>Workshops (Movement & Breathwork)</strong>
+                            </h2>
+                            <p className="text-lg text-[#3b3a2e] mb-6">
+                                Step into a safe, supportive space created just for you. Rooted in Co-Active coaching,
+                                these tailored sessions integrate somatic tools, breathwork, and conscious movement to
+                                meet you exactly where you are. Whether you’re navigating emotional blocks, seeking
+                               
+                            </p>
+                            <Link
+                                to="/contact"
+                                className="bg-[#3b3a2e] text-white px-6 py-3 rounded-lg font-semibold hover:bg-[#2d2c22] transition-colors "
+                            >
+                                Read more
+                            </Link>
+                            <h2 className="text-lg text-[#3b3a2e] mb-6 mt-6">
+                                <strong>Team Building Workshop</strong>
+                            </h2>
+                            <p className="text-lg text-[#3b3a2e] mb-6">
+                                Step into a safe, supportive space created just for you. Rooted in Co-Active coaching,
+                                these tailored sessions integrate somatic tools, breathwork, and conscious movement to
+                                meet you exactly where you are. Whether you’re navigating emotional blocks, seeking
+                                
+                            </p>
+                            <Link
+                                to="/contact"
+                                className="bg-[#3b3a2e] text-white px-6 py-3 rounded-lg font-semibold hover:bg-[#2d2c22] transition-colors "
+                            >
+                                Read more
+                            </Link>
+                             <h2 className="text-lg text-[#3b3a2e] mb-6 mt-6">
+                                <strong>Executive Coaching</strong>
+                            </h2>
+                            <p className="text-lg text-[#3b3a2e] mb-6">
+                                Step into a safe, supportive space created just for you. Rooted in Co-Active coaching,
+                                these tailored sessions integrate somatic tools, breathwork, and conscious movement to
+                                meet you exactly where you are. Whether you’re navigating emotional blocks, seeking
+                                
+                            </p>
+                            <Link
+                                to="/contact"
+                                className="bg-[#3b3a2e] text-white px-6 py-3 rounded-lg font-semibold hover:bg-[#2d2c22] transition-colors "
+                            >
+                                Read more
                             </Link>
                         </div>
                     </div>
--- src/pages/IndividualPage.jsx
+++ src/pages/IndividualPage.jsx
@@ -16,54 +16,48 @@ const individualPage = () => {
                         </div>
                         <div>
                             <h2 className="text-lg text-[#3b3a2e] mb-6">
-                                <strong>For Individuals</strong>
+                                <strong>1:1 Coaching</strong>
                             </h2>
                             <p className="text-lg text-[#3b3a2e] mb-6">
                                 Step into a safe, supportive space created just for you. Rooted in Co-Active coaching,
                                 these tailored sessions integrate somatic tools, breathwork, and conscious movement to
                                 meet you exactly where you are. Whether you’re navigating emotional blocks, seeking
-                                clarity, or reconnecting with your body and inner wisdom, you’ll be gently guided to
-                                transform from within.
                             </p>
-
-                            <ul className="text-lg text-[#3b3a2e] mb-6">
-                                <li>
-                                  <h3 className="mb-4 mt-4"><strong >Tools</strong></h3>  
-                                    <ul>
-                                        <li>Co-Active Coaching — explore your inner landscape and evoke insight</li>
-                                        <li>Conscious Movement — awaken body awareness and release tension</li>
-                                        <li>Breathwork — regulate your nervous system and open energy flow</li>
-                                        <li>Somatic Inquiry — access deeper emotional layers and memories</li>
-                                    </ul>
-                                </li>
-                                <li>
-                                  <h3 className="mb-4 mt-4"><strong>Benefits</strong></h3>  
-                                    <ul>
-                                        <li>Emotional release and integration</li>
-                                        <li>Nervous system regulation and stress relief</li>
-                                        <li>Deepened connection to body and intuition</li>
-                                        <li>Increased clarity, vitality, and presence</li>
-                                    </ul>
-                                </li>
-                            </ul>
                             <Link
                                 to="/contact"
-                                className="bg-[#3b3a2e] text-white px-6 py-3 rounded-lg font-semibold hover:bg-[#2d2c22] transition-colors"
+                                className="bg-[#3b3a2e] text-white px-6 py-3 rounded-lg font-semibold hover:bg-[#2d2c22] transition-colors "
                             >
-                                Contact Us
+                                Read more
                             </Link>
-                            <ul>
-                                <li></li>
-                            </ul>
-                            <p className="text-lg text-[#3b3a2e]  mb-8">
-                                This is your time — to feel, explore, and transform at your own pace, with compassionate
-                                support every step of the way.
+                            <h2 className="text-lg text-[#3b3a2e] mb-6 mt-6">
+                                <strong>Group Coaching Program</strong>
+                            </h2>
+                            <p className="text-lg text-[#3b3a2e] mb-6">
+                                Step into a safe, supportive space created just for you. Rooted in Co-Active coaching,
+                                these tailored sessions integrate somatic tools, breathwork, and conscious movement to
+                                meet you exactly where you are. Whether you’re navigating emotional blocks, seeking
+                               
                             </p>
                             <Link
                                 to="/contact"
-                                className="bg-[#3b3a2e] text-white px-6 py-3 rounded-lg font-semibold hover:bg-[#2d2c22] transition-colors"
+                                className="bg-[#3b3a2e] text-white px-6 py-3 rounded-lg font-semibold hover:bg-[#2d2c22] transition-colors "
                             >
-                                Contact Us
+                                Read more
+                            </Link>
+                            <h2 className="text-lg text-[#3b3a2e] mb-6 mt-6">
+                                <strong>Workshops</strong>
+                            </h2>
+                            <p className="text-lg text-[#3b3a2e] mb-6">
+                                Step into a safe, supportive space created just for you. Rooted in Co-Active coaching,
+                                these tailored sessions integrate somatic tools, breathwork, and conscious movement to
+                                meet you exactly where you are. Whether you’re navigating emotional blocks, seeking
+                                
+                            </p>
+                            <Link
+                                to="/contact"
+                                className="bg-[#3b3a2e] text-white px-6 py-3 rounded-lg font-semibold hover:bg-[#2d2c22] transition-colors "
+                            >
+                                Read more
                             </Link>
                         </div>
                     </div>
