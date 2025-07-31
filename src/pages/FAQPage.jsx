d12f78dfffca5281dffe59ff70d2ae853a5ba36a
Stefan Nieuwenburg~ <Nieuwenburg.stefan@gmail.com>
2025-07-31 10:49:52 +0200

    commit

--- src/pages/FAQPage.jsx
+++ src/pages/FAQPage.jsx
@@ -8,20 +8,15 @@ const FAQPage = () => {
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4">
             <div className="mx-auto mb-[60px] max-w-[520px] text-center lg:mb-20">
-              <span className="mb-2 block text-lg font-semibold text-primary">
-                FAQ
-              </span>
-              <h2 className="mb-4 text-3xl font-bold text-dark dark:text-white sm:text-[40px]/[48px]">
-                Any Questions? Look Here
-              </h2>
+              <h1 className="mb-4 text-3xl font-bold text-dark dark:text-white sm:text-[40px]/[48px]">
+                Frequently Asked Questions
+              </h1>
               <p className="text-base text-body-color dark:text-dark-6">
-                There are many variations of passages of Lorem Ipsum available
-                but the majority have suffered alteration in some form.
+                Here are some common questions about our coaching services. If you have more questions, feel free to reach out!
               </p>
             </div>
           </div>
         </div>
-
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4 lg:w-1/2">
             <AccordionItem
